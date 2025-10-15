ee50f58aae20c3ac2d3261af8c6d078826ebdf8f
hmyge225 <jean.mangoua225@gmail.com>
2025-10-15 16:36:36 +0000

    Landing page html/css/js

---
+++ app.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "100%";
+    document.body.style.overflow = "hidden";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0%";
+}
